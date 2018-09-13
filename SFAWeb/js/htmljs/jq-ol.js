086ec2e8a0e1d7e913077b6e3a86b1c57a461a28
k15122131474 <2591313550@qq.com>
2018-09-13 18:46:26 +0800

    logout

--- SFAWeb/js/htmljs/jq-ol.js
+++ SFAWeb/js/htmljs/jq-ol.js
@@ -8,18 +8,21 @@ function ioclick(a){
     para="";
 //    var sign = a.parent().var();
     while(a.id!=="menu"){
-        para=" <li class=\"breadcrumb-item\"><a href=\""+document.getElementById(a.id).childNodes[0]+"\" target=\"iframe-info\">"+ a.textContent+"<i class=\"fa fa-angle-right\"></i></li>"+para;
+        para=" <li class=\"breadcrumb-item\"><a href=\""+document.getElementById(a.id).childNodes[0]+"\" target=\"iframe-info\">"+ a.childNodes[0].textContent+"<i class=\"fa fa-angle-right\"></i></li>"+para;
         var id=document.getElementById(a.id).parentNode.id;
-        if(id==="") {
+        while(id==="") {
             id=document.getElementById(a.id).parentNode.parentNode.id;
         }
         a=document.getElementById(id);
    //     var a= $(a).parent();
     }
     //var para= " <li class=\"breadcrumb-item\"><a onclick=\"setiframe(a)\">"+ a.text+"<i class=\"fa fa-angle-right\"></i></li>"
+    alert(para);
     $("#ol").append(para);
 
 }
+
+
 function lodeiframe(){
     var h=document.getElementById("iframe-info");
     h.setAttribute('height',h.contentWindow.document.body.clientHeight);
